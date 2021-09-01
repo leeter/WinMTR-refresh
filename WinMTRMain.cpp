2e51f86621557e89053b9beebe9f06ed72cf3a72
leeter <leeterfp@gmail.com>
2021-09-01 11:18:06 -0600

    reduce memory by just using the new common controls

--- WinMTRMain.cpp
+++ WinMTRMain.cpp
@@ -35,6 +35,10 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 WinMTRMain WinMTR;
 
 //*****************************************************************************
